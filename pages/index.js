d6e626fddd7be7c9d883fc611e6ae3d8d47b6b74
ChessEngineSolution <53708652+tetrashop@users.noreply.github.com>
2025-10-21 14:55:09 +0330

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// در pages/index.js
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>خوش آمدید به تتراشاپ</h1>
+      {/* بقیه محتوا */}
+    </div>
+  );
+}
