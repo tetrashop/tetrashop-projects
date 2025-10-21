2b4cc1b9d3e87f6ec8211ca72c7739f3692b2a5a
ChessEngineSolution <53708652+tetrashop@users.noreply.github.com>
2025-10-21 14:33:37 +0330

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fa" dir="rtl">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
